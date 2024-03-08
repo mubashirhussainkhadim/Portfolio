2dbb3bda7ec9a84e9eaba7fd752750129e19eaf7
Mubashir <mubashir@192.168.10.36>
2024-03-08 16:36:11 +0500

    Second  commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet , FontAwesomeModule],
+  template: `<router-outlet></router-outlet>`,
+})
+export class AppComponent {}
